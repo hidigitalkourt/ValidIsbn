58336e2ff1c1ee279b9f74c124fd1982652fc9ff
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-05 15:55:12 -0700

    Add most degenerate case test

--- ValidIsbnTest/ValidIsbnTest.cs
+++ ValidIsbnTest/ValidIsbnTest.cs
@@ -8,6 +8,16 @@ namespace ValidIsbnTest
     public class ValidIsbnTest
     {
         [Fact]
+        public void ReturnFalseForEmptyIsbn()
+        {
+            var isbnInput = "";
+            var expected = false;
+            var actual = ValidateIsbn.IsValidIsbn(isbnInput);
+
+            actual.Should().Be(expected);
+        }
+        
+        [Fact]
         public void ReturnTrueForValidIsbnNoSpaces()
         {
             var isbnInput = "0471958697";
