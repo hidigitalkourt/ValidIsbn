1425256f1fad812c394b6e4bf9f9da11c49f1938
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-05 20:01:30 -0700

    Passes test with disregard for dashes

--- ValidIsbn/ValidIsbn.cs
+++ ValidIsbn/ValidIsbn.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ValidIsbn
 {
@@ -6,10 +7,22 @@ namespace ValidIsbn
     {
         public static bool IsValidIsbn(string isbn)
         {   
+            var isbnCharList = new string []{};
+            string processedIsbn;
             if( isbn == null || isbn == "")
             {
                 return false;
             }
+            if( isbn.Contains("-"))
+            {
+                isbnCharList = isbn.Split('-');
+                processedIsbn = String.Join("",isbnCharList);
+                if (processedIsbn.Length != 13) 
+                {
+                    return false;
+                }
+                return true;
+            } 
             if( isbn.Length != 13)
             {
                 return false;
