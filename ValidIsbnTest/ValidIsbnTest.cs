1d809537c58107190701519fc3678256d9df006f
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-17 15:37:13 -0700

    Fixes accidental spacing in test

--- ValidIsbnTest/ValidIsbnTest.cs
+++ ValidIsbnTest/ValidIsbnTest.cs
@@ -65,7 +65,7 @@ namespace ValidIsbnTest
         public void ReturnsSumOfIsbnDigitsAlternatelyMulitipliedByOneOrThree()
         {
             //   131 3 131 31313
-            var isbnInput = "978 0262134729";
+            var isbnInput = "9780262134729";
             var expected = 9 + (7 * 3) + 8 + (0 * 3) + 2 + (6 * 3) + 2 + (1 * 3) + 3 + (4 * 3) + 7 + (2 * 3);
             var actual = ValidateIsbn.SumsAlternatingMulitpliersOfDigits(isbnInput);
 
