8010dabb1d451bfd2a852e4b6fa1b2c0740e4540
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-18 20:18:54 -0700

    Removes digit checking code from valid isbn

--- ValidIsbn/ValidIsbn.cs
+++ ValidIsbn/ValidIsbn.cs
@@ -18,30 +18,6 @@ namespace ValidIsbn
         private static string ReducedIsbn(string isbn)
         {
             return isbn.Replace("-", "").Replace(" ", "");
-        }
-
-        private static int GetSumOfDigitsWithAlternatingMultiplier(string isbn)
-        {
-            var index = 1;
-            var sum = 0;
-
-            foreach (char ch in isbn)
-            {
-                if (index < 13)
-                {
-                    int num = int.Parse(Char.ToString(ch));
-                    sum += ((index % 2) == 0) ? (num * 3) : num;
-                }
-                index++;
-            }
-            return sum;
-        }
-
-        public static bool HasValidCheckDigitForLengthThirteen(string isbn)
-        {
-            var checkDigit = (10 - (GetSumOfDigitsWithAlternatingMultiplier(isbn) % 10)) % 10;
-            var lastDigit = isbn[12];
-            return checkDigit.ToString() == lastDigit.ToString();
-        }
+        }    
     }
 }
