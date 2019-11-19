0a6fc1c24a0beaac21e2cb93952a0decad3e9b17
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-18 19:57:52 -0700

    Fixes merge conflicts for ValidIsbn

--- ValidIsbnTest/DigitCheckerTest.cs
+++ ValidIsbnTest/DigitCheckerTest.cs
@@ -5,8 +5,40 @@ using FluentAssertions;
 
 namespace ValidIsbnTest
 {
-    public class DigitChecker
+    public class DigitCheckerTest
     {
         
+
+        [Fact]
+        public void ReturnsModuloOfSummedDigits()
+        {
+            var isbnInput = "9780262134729";
+            var expected = 91 % 10; 
+            var actual = DigitChecker.ModulusTenOfResult(isbnInput);
+
+            actual.Should().Be(expected);
+
+        }
+
+        [Fact]
+        
+        public void ReturnsModuloOfResultSubtractedFromTen()
+        {
+            var isbnInput = "9780262134729";
+            var expected = (10 - (91 % 10)) % 10;
+            var actual = DigitChecker.ModulusTenOfSubtractedResult(isbnInput);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsFalseForInvalidCheckDigit()
+        {
+            var isbn = "9780262134723";
+            var expected = false;
+            var actual = DigitChecker.IsValidIsbn(isbn);
+
+            actual.Should().Be(expected);
+        }
     }
 }
