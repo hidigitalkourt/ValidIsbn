7ca11425bae1a1e3b7f60120f7f23d4d05332e53
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-18 20:33:44 -0700

    Adds digit checking code to new class

--- ValidIsbn/DigitChecker.cs
+++ ValidIsbn/DigitChecker.cs
@@ -5,10 +5,29 @@ namespace ValidIsbn
 {
     public static class DigitChecker
     {
-        public static bool GetCheckDigit(string isbn)
+        public static bool HasValidCheckDigitForLengthThirteen(string isbn)
         {
-            return false;
-        }        
+            var checkDigit = (10 - (GetSumOfDigitsWithAlternatingMultiplier(isbn) % 10)) % 10;
+            var lastDigit = isbn[12];
+            return checkDigit.ToString() == lastDigit.ToString();
+        }     
+
+        public static int GetSumOfDigitsWithAlternatingMultiplier(string isbn)
+        {
+            var index = 1;
+            var sum = 0;
+
+            foreach (char ch in isbn)
+            {
+                if (index < 13)
+                {
+                    int num = int.Parse(Char.ToString(ch));
+                    sum += ((index % 2) == 0) ? (num * 3) : num;
+                }
+                index++;
+            }
+            return sum;
+        }   
     }
     
 }
