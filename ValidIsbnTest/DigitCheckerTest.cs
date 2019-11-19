fc179a3acf2e23c13cd0c14535dcf04ce9786331
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-18 20:15:59 -0700

    Add digit checker tests

--- ValidIsbnTest/DigitCheckerTest.cs
+++ ValidIsbnTest/DigitCheckerTest.cs
@@ -7,28 +7,15 @@ namespace ValidIsbnTest
 {
     public class DigitCheckerTest
     {
-        
-
-        [Fact]
-        public void ReturnsModuloOfSummedDigits()
+        public void ReturnsSumOfIsbnDigitsAlternatelyMulitipliedByOneOrThree()
         {
+            //   131 3 131 31313
             var isbnInput = "9780262134729";
-            var expected = 91 % 10; 
-            var actual = DigitChecker.ModulusTenOfResult(isbnInput);
+            var expected = 9 + (7 * 3) + 8 + (0 * 3) + 2 + (6 * 3) + 2 + (1 * 3) + 3 + (4 * 3) + 7 + (2 * 3);
+            var actual = DigitChecker.GetSumOfDigitsWithAlternatingMultiplier(isbnInput);
 
             actual.Should().Be(expected);
 
-        }
-
-        [Fact]
-        
-        public void ReturnsModuloOfResultSubtractedFromTen()
-        {
-            var isbnInput = "9780262134729";
-            var expected = (10 - (91 % 10)) % 10;
-            var actual = DigitChecker.ModulusTenOfSubtractedResult(isbnInput);
-
-            actual.Should().Be(expected);
         }
 
         [Fact]
@@ -36,7 +23,7 @@ namespace ValidIsbnTest
         {
             var isbn = "9780262134723";
             var expected = false;
-            var actual = DigitChecker.IsValidIsbn(isbn);
+            var actual = DigitChecker.HasValidCheckDigitForLengthThirteen(isbn);
 
             actual.Should().Be(expected);
         }
