17f8312ba1450260329c4d86fd162b46fc18dd40
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-11-21 00:15:10 -0700

    Adds digit checking for isbn 10

--- ValidIsbn/DigitChecker.cs
+++ ValidIsbn/DigitChecker.cs
@@ -7,44 +7,38 @@ namespace ValidIsbn
     {
         public static bool HasValidCheckDigit(string isbn)
         {
-            string checkDigit;
-            char lastDigit;
-            if (isbn.Length == 13)
-            {
-                checkDigit = (10 - (GetSumOfMultipliedDigits(isbn) % 10) % 10).ToString();
-                lastDigit = isbn[12];
-            }
-            else
-            {
-                checkDigit = (GetSumOfMultipliedDigits(isbn) % 11).ToString();
-                lastDigit = isbn[9];
-            }
-            return checkDigit == lastDigit.ToString();
+            return isbn.Length == 13 ? 
+            GetCheckDigitForIsbnThirteen(isbn).ToString() == isbn[12].ToString() :
+            GetCheckDigitForIsbnTen(isbn).ToString() == isbn[9].ToString();
         }     
-
         public static int GetSumOfMultipliedDigits(string isbn)
         {
             var index = 1;
             var sum = 0;
 
-            foreach (char ch in isbn)
+            foreach (char ch in isbn.Substring(0,isbn.Length -1))
             {
                 int num = int.Parse(Char.ToString(ch));
-                if (index < isbn.Length)
+                if( isbn.Length == 13)
                 {
-                    if( isbn.Length == 13)
-                    {
-                        sum += ((index % 2) == 0) ? (num * 3) : num;
-                    }
-                    else
-                    {
-                        sum += (index * num);
-                    }
+                    sum += ((index % 2) == 0) ? (num * 3) : num;
+                }
+                else
+                {
+                    sum += (index * num);
                 }
                 index++;
             }
             return sum;
         }   
+        public static string GetCheckDigitForIsbnTen(string isbn)
+        {
+            return (GetSumOfMultipliedDigits(isbn) % 11) == 10 ? "X" :(GetSumOfMultipliedDigits(isbn) % 11).ToString();
+        }
+        public static string GetCheckDigitForIsbnThirteen(string isbn)
+        {
+            return (10 - (GetSumOfMultipliedDigits(isbn) % 10) % 10).ToString();
+        }
     }
     
 }
